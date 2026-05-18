786cd2adb5504ce5822e5d7b5c8d3c7582f4ff9b
Tarun Sharma <tarunsharama07821@gmail.com>
2026-05-18 23:36:33 +0530

    Initialize Spring Boot service with PostgreSQL and Redis config

---
+++ service/src/test/java/com/throttlex/ThrottlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.throttlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrottlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
